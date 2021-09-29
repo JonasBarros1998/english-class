646eae69c4b4391c8f93fb1837f8a9e58c1cef3d
Jonas Barros <Jonas_barros@outlook.com>
2021-09-28 21:51:10 -0300

    Montando o design do card

---
+++ src/components/Forms/CreateLists.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+import { Input, Box } from 'native-base';
+
+function CreateLists() {
+  return (
+    <Box 
+      width="100%" 
+      flex={1}
+      justifyContent="flex-start" 
+      alignItems="center">
+      <Box
+        margin={2}
+        bg="#fff"
+        width="95%"
+        rounded="lg"
+        padding="3"
+        shadow={1}
+        _web={{
+          shadow: 2,
+          borderWidth: 0,
+        }}
+      >
+
+        <Input
+          variant="underlined"
+          placeholder="Palavra"
+          fontSize={16}
+          placeholderTextColor="#78716c"
+          width="100%"
+          fontWeight={600}
+          _focus={{
+            borderBottomColor: '#000'
+          }} />
+
+        <Input
+          variant="underlined"
+          placeholder="Tradução"
+          fontSize={16}
+          placeholderTextColor="#78716c"
+          width="100%"
+          fontWeight={600}
+          _focus={{
+            borderBottomColor: '#000'
+          }}
+           />
+
+        <Input
+          variant="underlined"
+          placeholder="Contexto"
+          fontSize={16}
+          placeholderTextColor="#78716c"
+          width="100%"
+          fontWeight={600}
+          _focus={{
+            borderBottomColor: '#000'
+          }} />
+      </Box>
+    </Box>
+  );
+}
+
+export default CreateLists;
---
+++ src/screen/CreateLists.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {CreateLists} from '@components/Forms';
+
+function CreateListsScreen() {
+  return (<CreateLists />);
+}
+
+export default CreateListsScreen;
