f468b81f2f91dac025f2968407b6486fc238ef4c
Jonas Barros <Jonas_barros@outlook.com>
2021-10-03 21:43:31 -0300

    funcionalidade de criação e edição dos cards

---
+++ src/components/Forms/cards.ts
@@ -0,0 +1,42 @@
+type card = {
+  id: number,
+  word: string,
+  translation: string,
+  context: string,
+};
+
+type inputType = 'word' | 'translation' | 'context';
+
+const initialCard = {
+  id: Math.floor(Math.random() * 10000),
+  word: "",
+  translation: "",
+  context: ""
+};
+
+const listAllCards: Array<card> = [initialCard];
+
+function addNewCardEmpty() {
+  listAllCards.push({
+    id: Math.floor(Math.random() * 10000),
+    word: "",
+    translation: "",
+    context: ""
+  });
+}
+
+function updateForm(input: string, card: card, inputType: inputType) {
+  card[inputType] = input;
+
+  listAllCards.filter((item, index) => {
+    if (item.id === card.id) {
+      listAllCards[index] = item;
+    }
+  });
+}
+
+function getListCards() {
+  return listAllCards;
+}
+
+export {addNewCardEmpty, updateForm, getListCards};
