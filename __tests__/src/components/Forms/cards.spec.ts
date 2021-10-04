d6414806e4868b271665273aaae2b1ecafe0deb0
Jonas Barros <Jonas_barros@outlook.com>
2021-10-03 21:44:41 -0300

    teste unitario para cards

---
+++ __tests__/src/components/Forms/cards.spec.ts
@@ -0,0 +1,44 @@
+import {addNewCardEmpty, 
+  getListCards, 
+  updateForm} from '@components/Forms/cards'; 
+
+test('should add new card', function() {
+  addNewCardEmpty();
+
+  getListCards().map(function(item) {
+    expect(item).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        word: expect.any(String),
+        translation: expect.any(String),
+        context: expect.any(String),
+      }),
+    );
+  });
+});
+
+test('should return a card', function() {
+  const [card] = getListCards();
+  expect(card).toEqual(
+    expect.objectContaining({
+      id: expect.any(Number),
+      word: expect.any(String),
+      translation: expect.any(String),
+      context: expect.any(String),
+    }),
+  );
+});
+
+test('should edit a card', function() {
+  const [getCard] = getListCards();
+
+  updateForm('editing text', getCard, 'word');
+  expect(getCard).toEqual(
+    expect.objectContaining({
+    id: expect.any(Number),
+    word: 'editing text',
+    translation: expect.any(String),
+    context: expect.any(String),
+  }),
+  );
+});
