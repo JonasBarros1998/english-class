bedee29224b0483858df0363e1b1309e6f0c13d7
Jonas Barros <Jonas_barros@outlook.com>
2021-10-07 20:54:33 -0300

    metodo para enviar as lista ao banco de dados

--- src/database/repository/insert.ts
+++ src/database/repository/insert.ts
@@ -1,20 +1,23 @@
 import {db as connection} from '../connection';
 
-async function insert(datas: any, where: string) {
+async function insert(datas: Array<any>, where: string): Promise<any[]> {
   return connection()
     .then(function (database) {
-      database
-        .ref(where)
-        .set(datas)
-        .then(function (response) {
-          console.log(response);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+      const reference = database.ref(where);
+      return datas.map((data: any) => {
+        reference
+          .push()
+          .set(data)
+          .then(function () {
+            return data;
+          })
+          .catch(function (error) {
+            return Promise.reject(new Error(error.message));
+          });
+      });
     })
     .catch(function (erro) {
-      Promise.reject(new Error(erro.message));
+      return Promise.reject(new Error(erro.message));
     });
 }
 
