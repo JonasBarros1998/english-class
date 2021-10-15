ba549982a0cd14ad9efc1c3f1bb084f351f59f7d
Jonas Barros <Jonas_barros@outlook.com>
2021-10-14 22:35:05 -0300

    novo componente para renderizar os detalhes de cada card

---
+++ src/components/Cards/CardDetails.tsx
@@ -0,0 +1,111 @@
+import React, {useEffect, useState} from 'react';
+
+import {Center, Text, Box, FlatList} from 'native-base';
+import {select} from '@database/repository/search';
+
+type cards = {
+  cards: {
+    context: string;
+    id: 0;
+    translation: string;
+    word: string;
+  }[];
+};
+
+type params = {
+  context: string;
+  id: number;
+  translation: string;
+  word: string;
+};
+
+type cardItemParam = {
+  cardItem: {
+    id: string;
+    idioma: string;
+    name: string;
+    quantityWords: number;
+  };
+};
+
+async function loadUserList(cardItemId: string): Promise<cards> {
+  return select(`123456789/lists/${cardItemId}`)
+    .then(function (response) {
+      console.log('loadUserList >>>', response.toJSON());
+      return response.toJSON() as cards;
+    })
+    .catch(function (error) {
+      return Promise.reject(new Error(error));
+    });
+}
+
+function CardDetails({context, id, translation, word}: params) {
+  return (
+    <Center alignItems="flex-start" testID="publicCard">
+      <Box
+        bgColor="white"
+        shadow={1}
+        rounded="lg"
+        maxWidth="90%"
+        width="90%"
+        mt={3}
+        mb={2}
+        p={2}>
+        <Text fontFamily="body" fontWeight={600} fontSize={19} pb={1} bold>
+          {word}
+        </Text>
+
+        <Text
+          fontFamily="body"
+          fontWeight={600}
+          fontSize={17}
+          pb={1}
+          testID="quantityWords">
+          {context}
+        </Text>
+
+        <Text fontFamily="body" fontWeight={600} fontSize={17}>
+          {translation}
+        </Text>
+      </Box>
+    </Center>
+  );
+}
+
+const userCard = {
+  cards: [
+    {
+      context: '',
+      id: 0,
+      translation: '',
+      word: '',
+    },
+  ],
+};
+
+function RenderCards({cardItem}: cardItemParam) {
+  const {id} = cardItem;
+  console.log('id >>>', id);
+  const [card, setCard] = useState(userCard);
+  useEffect(
+    function () {
+      loadUserList(id).then(function (response) {
+        console.log('Vixxxx', response);
+        setCard(response);
+      });
+    },
+    [id],
+  );
+
+  return (
+    <FlatList
+      data={card.cards}
+      renderItem={({item}) => {
+        return <CardDetails {...item} />;
+      }}
+      keyExtractor={({id}) => id.toString()}
+    />
+  );
+}
+
+export default RenderCards;
