900d6a20a888b525989dcbdddbb374e954134329
Jonas Barros <Jonas_barros@outlook.com>
2021-11-13 17:30:54 -0300

    novo componente de modal

--- src/components/Modal/ModalComponent.tsx
+++ src/components/Modal/ModalComponent.tsx
@@ -1,33 +1,47 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import {Modal, Button, ScrollView, Text} from 'native-base';
+import {useDispatch} from 'react-redux';
+// import {onOff} from '@pubsub/onOffSlice';
+import {useSelector} from 'react-redux';
+import {onOff} from '@pubsub/onOffSlice';
 
 type param = {
   visible: boolean;
-  /*setmodalClosed: Function;*/
   size?: 'md' | 'xs' | 'sm' | 'lg' | 'xl' | 'full';
   content?: string;
 };
 
-function ModalComponent({visible, /*setmodalClosed*/ size, content}: param) {
-  const [modalClose, setModalClosed] = useState(visible);
-  /*
-  useEffect(() => {
-    if (modalClose === true) {
-      setModalClosed(true);
-      console.log(visible, modalClose);
-      return;
-    }
-    setModalClosed(false);
-  }, [modalClose, visible]);*/
+type status = {
+  onOff: {
+    status: boolean;
+    message: string;
+  }[];
+};
 
+function ModalComponent() {
+  const [closeModal, setModalClosed] = useState(false);
+  const [messageModal, setMessageModal] = useState('');
+  const dispatch = useDispatch();
+
+  const statusModal = useSelector((state: status) => state.onOff);
+
+  const statusModalCallback = useCallback(() => {
+    const [{status, message}] = statusModal;
+    setModalClosed(() => status);
+    setMessageModal(() => message);
+  }, [statusModal]);
+
+  useEffect(() => {
+    statusModalCallback();
+  });
   return (
-    <Modal isOpen={modalClose} onClose={setModalClosed} size={size}>
+    <Modal isOpen={closeModal} onClose={setModalClosed}>
       <Modal.Content maxH="212">
         <Modal.CloseButton />
         <Modal.Header>Erro</Modal.Header>
         <Modal.Body>
           <ScrollView>
-            <Text>{content}</Text>
+            <Text>{messageModal}</Text>
           </ScrollView>
         </Modal.Body>
         <Modal.Footer>
@@ -36,7 +50,7 @@ function ModalComponent({visible, /*setmodalClosed*/ size, content}: param) {
               variant="ghost"
               colorScheme="blueGray"
               onPress={() => {
-                setModalClosed(false);
+                dispatch(onOff({status: false, message: ''}));
               }}>
               Fechar
             </Button>
