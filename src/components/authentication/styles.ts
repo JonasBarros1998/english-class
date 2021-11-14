f9e194842df9b4881e3975dc8346826683b3d7d2
Jonas Barros <Jonas_barros@outlook.com>
2021-11-13 21:05:08 -0300

    adicionando estilos do componente em um arquivo separado

---
+++ src/components/authentication/styles.ts
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  signInButton: {
+    width: 200,
+    height: 50,
+  },
+  status: {
+    marginVertical: 20,
+  },
+  loggedinMessage: {
+    fontSize: 20,
+    color: 'tomato',
+  },
+});
+
+export {styles};
