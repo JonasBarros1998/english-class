a3b3e4236058d00c32667cf377056e6a6fbf1bcd
Jonas Barros <Jonas_barros@outlook.com>
2021-11-15 12:45:52 -0300

    criando testes unitarios do async storage

---
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -0,0 +1,6 @@
+// import mockAsyncStorage from '@react-native-async-storage/async-storage';
+
+// jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+
+export default from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+
