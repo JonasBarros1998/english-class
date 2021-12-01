7d2b3e342b993c28ffb3b4ae1af57e451cdfba9a
Jonas Barros <Jonas_barros@outlook.com>
2021-12-01 10:49:50 -0300

    savando listas publicas do usuario

--- src/components/Save/useCase/saveUserList.ts
+++ src/components/Save/useCase/saveUserList.ts
@@ -3,15 +3,28 @@ import {storageGetItem} from '@storage/getItem';
 import {USER_STORAGE} from '@global/constants';
 import {userInfo} from '@global/types/userInfo';
 
-async function saveUserList(listIsPrivate: boolean, datas: any) {
+async function saveUserList(listIsPrivate: boolean, datas: any[]) {
+  const storage = await storageGetItem(USER_STORAGE);
   if (listIsPrivate) {
-    const storage = await storageGetItem(USER_STORAGE);
     if (storage !== null) {
       const userData = JSON.parse(storage) as userInfo;
       const where = `privateList/${userData.uid}`;
       await insert(datas, where);
+      return;
     }
   }
+
+  const userData = JSON.parse(storage) as userInfo;
+  const [data] = datas;
+  const userListWithUid = Object.defineProperty(data, 'userId', {
+    enumerable: true,
+    writable: true,
+    configurable: true,
+    value: userData.uid,
+  });
+
+  await insert([userListWithUid], 'publicList');
+  return;
 }
 
 export {saveUserList};
