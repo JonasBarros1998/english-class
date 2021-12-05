b0e00458590ccfa1ca82f8282e6ed9169c738e7d
Jonas Barros <Jonas_barros@outlook.com>
2021-12-04 21:45:36 -0300

    carregando listas offline

--- src/components/Lists/privateLists/useCase/loadPrivateList.ts
+++ src/components/Lists/privateLists/useCase/loadPrivateList.ts
@@ -1,45 +1,74 @@
 import {selectWithLimit} from '@database/repository/search';
 import {storageGetItem} from '@storage/index';
-import {USER_STORAGE} from '@global/constants';
+import {USER_LIST, USER_STORAGE} from '@global/constants';
 import {userList} from '@global/types/userList';
 import {userInfo} from '@global/types/userInfo';
 
+import {searchUserListInStorage} from '../../offline/searchUserListInStorage';
+import {addUserListInStorage} from '../../offline/addUserListInStorage';
+
 async function loadPrivateList(userId?: string) {
-  let where = '';
   const datasList: userList[] = [];
-  if (typeof userId !== 'undefined') {
-    where = `privateList/${userId}`;
-  } else {
-    const loadUserDataInLocalstorage = await loadUserId();
-    where = `privateList/${loadUserDataInLocalstorage.uid}`;
-  }
 
-  await selectWithLimit(where, 10)
-    .then(response => {
-      response.forEach(list => {
-        const dados = list.toJSON();
-        if (dados !== null) {
-          const datasUserList = dados as userList;
-          datasList.push({
-            id: response.key as string,
-            listTitle: datasUserList.listTitle,
-            cards: datasUserList.cards,
-            quantity: datasUserList.quantity,
-          });
-          return;
-        }
-        return;
+  const queryString = await where(userId);
+
+  const checkUserList = await checkUserListOffline();
+
+  if (checkUserList === true) {
+    await selectWithLimit(queryString, 10)
+      .then(response => {
+        response.forEach(list => {
+          const dados = list.toJSON();
+          if (dados !== null) {
+            const datasUserList = dados as userList;
+            datasList.push({
+              id: response.key as string,
+              listTitle: datasUserList.listTitle,
+              cards: datasUserList.cards,
+              quantity: datasUserList.quantity,
+            });
+            console.log(datasList, response.key);
+          }
+        });
+      })
+      .catch(function (erro) {
+        return Promise.reject(new Error(erro.message));
       });
-    })
-    .catch(function (erro) {
-      return Promise.reject(new Error(erro.message));
-    });
-
-  return datasList;
+    addUserListInStorage(datasList);
+    return datasList;
+  }
+  console.log(await loadUserListOffline());
+  // return await loadUserListOffline();
 }
 
 async function loadUserId(): Promise<userInfo> {
   return await storageGetItem(USER_STORAGE).then(user => JSON.parse(user));
+}
+
+async function checkUserListOffline() {
+  return searchUserListInStorage(USER_LIST)
+    .then(function (user) {
+      if (user === null) {
+        return false;
+      }
+      return true;
+    })
+    .catch(function (error) {
+      return Promise.reject(error);
+    });
+}
+
+async function loadUserListOffline(): Promise<userList[]> {
+  return await searchUserListInStorage(USER_LIST);
+}
+
+async function where(userId?: string) {
+  if (typeof userId !== 'undefined') {
+    return `privateList/${userId}`;
+  } else {
+    const loadUserDataInLocalstorage = await loadUserId();
+    return `privateList/${loadUserDataInLocalstorage.uid}`;
+  }
 }
 
 export {loadPrivateList};
