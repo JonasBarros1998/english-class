a9805d5319e3bfaafe6bdc5cb9072259a559a890
Jonas Barros <Jonas_barros@outlook.com>
2021-12-10 21:12:10 -0300

    excluindo cache da aplicação

--- src/pubsub/store.ts
+++ src/pubsub/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import onOff from './onOffSlice';
 import userInfo from './userInfoSlice';
+import userListOffline from './reducers/userListOffline';
 
 export default configureStore({
   reducer: {
     onOff,
     userInfo,
+    userListOffline,
   },
 });
