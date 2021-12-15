88b5197b21f673dfdf8e0b02fef48e20658da256
Jonas Barros <Jonas_barros@outlook.com>
2021-12-14 22:17:27 -0300

    refatorando componente de apresentação de detalhes da lista

--- src/components/ListDetails/index.tsx
+++ src/components/ListDetails/index.tsx
@@ -1,63 +1,24 @@
-import React, {useState, useEffect} from 'react';
-import {Pressable} from 'react-native';
+import React from 'react';
+import {FlatList} from 'react-native';
 
-import {Box, Center, Text} from 'native-base';
+import {Box, Center} from 'native-base';
+import {Card} from '@components/Cards/index';
+import {createCard} from '@global/types/cards';
 
-type paramsListDetails = {
-  word: string;
-  translation: string;
-  phrase: string;
-};
-
-function ListDetails({word, translation, phrase}: paramsListDetails) {
-  const [bgCard, setBgCard] = useState('white');
-
-  const [userPhrase, setUserPhrase] = useState(phrase);
-
-  useEffect(() => {
-    if (phrase.length > 65) {
-      const textFormatted = phrase.slice(0, 64);
-      setUserPhrase(textFormatted.padEnd(67, '...'));
-      return;
-    }
-    return setUserPhrase(phrase);
-  }, [phrase]);
-
-  const onPressButton = () => {
-    setBgCard('#F2F2F2');
-    setUserPhrase(phrase);
-  };
-
-  const outPressButton = () => setBgCard('white');
-
+function ListDetails(cards: createCard[]) {
   return (
-    <Pressable
-      onPress={() => onPressButton()}
-      onPressOut={() => outPressButton()}>
-      <Center alignItems="flex-start">
-        <Box
-          bgColor={bgCard}
-          shadow={2}
-          rounded="lg"
-          maxWidth="90%"
-          width="90%"
-          mt={3}
-          mb={2}
-          p={2}>
-          <Text fontFamily="body" fontWeight={800} fontSize={19} pb={1}>
-            {word}
-          </Text>
-
-          <Text fontFamily="body" fontWeight={600} fontSize={17} pb={1}>
-            {translation}
-          </Text>
-
-          <Text fontFamily="body" fontWeight={600} fontSize={17}>
-            {userPhrase}
-          </Text>
-        </Box>
-      </Center>
-    </Pressable>
+    <Center alignItems="flex-start">
+      <Box
+        shadow={2}
+        rounded="lg"
+        maxWidth="90%"
+        width="90%"
+        mt={3}
+        mb={2}
+        p={2}>
+        <FlatList data={cards} renderItem={({item}) => <Card {...item} />} />
+      </Box>
+    </Center>
   );
 }
 
