9da191d44b1bf2039b4602557b41572ceafa1630
Jonas Barros <Jonas_barros@outlook.com>
2021-12-18 20:06:34 -0300

    criando novas variaveis

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -1,2 +1,5 @@
+import {Dimensions} from 'react-native';
+
 export const USER_STORAGE = '@USER_ID';
 export const USER_LIST = '@USER_LIST';
+export const WIDTH_SCREEN = Dimensions.get('window').width;
