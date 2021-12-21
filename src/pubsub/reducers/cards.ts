bdd2b1063974f6970e8716b05007921cfc348d71
Jonas Barros <Jonas_barros@outlook.com>
2021-12-20 22:08:18 -0300

    criando novo reducer para gerenciamento de estados

---
+++ src/pubsub/reducers/cards.ts
@@ -0,0 +1,19 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+export const cardsSlice = createSlice({
+  name: 'cards',
+  initialState: [],
+  reducers: {
+    addCardEmpty: (state: any, action: any) => {
+      state.push(action.payload);
+    },
+
+    deleteCard: (state, action) => {
+      throw 'METHOD NOT IMPLEMENTED';
+    },
+  },
+});
+
+export const {addCardEmpty} = cardsSlice.actions;
+
+export default cardsSlice.reducer;
--- src/pubsub/slices.ts
+++ src/pubsub/slices.ts
@@ -1,4 +1,5 @@
 import {onOff} from './onOffSlice';
 import {loggedUser} from './loggedUser';
+import {addCardEmpty} from './reducers/cards';
 
-export {onOff, loggedUser};
+export {onOff, loggedUser, addCardEmpty};
