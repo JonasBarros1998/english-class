e458edd36972db5e338fcdfc62520dc21f8cd6b4
Jonas Barros <Jonas_barros@outlook.com>
2022-01-10 16:44:48 -0300

    create new methods for search user public list

--- src/components/Lists/publicLists/useCase/loadPublicList.ts
+++ src/components/Lists/publicLists/useCase/loadPublicList.ts
@@ -5,64 +5,51 @@ import {userList as typeUserList} from '@global/types/userList';
 import {USER_STORAGE} from '@global/constants';
 import {storageGetItem} from '@storage/getItem';
 
-async function loadPublicListOfUserLogged(): Promise<userList[] | null> {
+async function toLoadPublicListOfTheUserLogged(quantity?: number) {
   const datasOfUser = await toLoadDatasOfUser();
-  loadAllPublicList();
-  const where = `publicList/${datasOfUser.uid}`;
 
   const datasOfTheList: typeUserList[] = [];
 
-  return select(where)
-    .then(function (response) {
-      const datas = response.toJSON();
-      if (datas === null) {
-        return null;
-      }
-
-      response.forEach(function (response) {
-        const datasPublicList = response.toJSON() as typeUserList;
-        datasOfTheList.push({
-          id: datasPublicList.id,
-          listTitle: datasPublicList.listTitle,
-          cards: datasPublicList.cards,
-          quantity: datasPublicList.quantity,
-          user: {
-            id: datasPublicList.user.id,
-            userName: datasPublicList.user.userName,
-            photoUrl: datasPublicList.user.photoUrl,
-          },
+  await Promise.all(
+    datasOfUser.lists.publicLists.map(async function (listId) {
+      const where = `publicList/${listId}`;
+      await select(where, quantity)
+        .then(function (response) {
+          const datas = response.toJSON();
+          if (datas === null) {
+            return null;
+          }
+          datasOfTheList.push(datas as typeUserList);
+        })
+        .catch(function (error) {
+          throw new Error(error.message);
         });
-      });
+    }),
+  );
 
-      return datasOfTheList;
-    })
-    .catch(function (error) {
-      throw new Error(error.message);
-    });
+  return datasOfTheList;
 }
 
-async function loadAllPublicList() {
-  const datasOfUser = await toLoadDatasOfUser();
-  const where = `publicList/${datasOfUser.uid}`;
+async function loadAllPublicListOfTheUserLogged() {
+  const datasOfTheList: typeUserList[] = [];
 
-  return select(where)
+  const where = 'publicList/';
+
+  await select(where)
     .then(function (response) {
-      const datas = response.toJSON();
-      if (datas === null) {
-        return null;
-      }
-      const rp = response.forEach(function (listItem: any) {
-        return listItem;
+      response.forEach(item => {
+        const datas = item.toJSON() as userList;
+        datasOfTheList.push(datas);
       });
-      console.log(rp);
     })
     .catch(function (error) {
       throw new Error(error.message);
     });
+  return datasOfTheList;
 }
 
 async function toLoadDatasOfUser(): Promise<userInfo> {
   return await storageGetItem(USER_STORAGE);
 }
 
-export {loadPublicListOfUserLogged, loadAllPublicList};
+export {toLoadPublicListOfTheUserLogged, loadAllPublicListOfTheUserLogged};
