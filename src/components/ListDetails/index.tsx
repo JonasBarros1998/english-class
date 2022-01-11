423d752f7f0c343aa79dfbcce8d4c4b7854c99a9
Jonas Barros <Jonas_barros@outlook.com>
2022-01-11 10:44:08 -0300

    desabling title of the list if user isn't owner the list

--- src/components/ListDetails/index.tsx
+++ src/components/ListDetails/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState, useCallback, useEffect} from 'react';
 import {Pressable} from 'react-native';
-import {Box, Input, Flex} from 'native-base';
+import {Box, Input, Flex, Text} from 'native-base';
 import {useSelector, useDispatch} from 'react-redux';
 import {userList as typeUserList} from '@global/types/userList';
 import Done from '@components/Svgs/Done';
@@ -10,6 +10,7 @@ import {validListTitle} from './useCase/validListTitle';
 import {updateAllCards} from '@pubsub/reducers/listOfCards';
 import Cards from '@components/Cards/AnimatedCards/Cards';
 import {countTotalOfCards} from './useCase/countQuantityTotalOfCards';
+import {theUserCanToEditTheList} from './useCase/theUserCanToEditTheList';
 
 type param = {
   userList: typeUserList;
@@ -19,6 +20,8 @@ type param = {
 function ListDetails(props: param) {
   const [titleList, setTitleList] = useState(props.userList.listTitle);
   const [visible, setVisible] = useState(false);
+  const [theUserCanToEditListHis, setTheUserCanToEditListHis] =
+    useState<boolean>();
 
   const datasOfList = useSelector(({listOfCards}: any) => listOfCards);
   const dispatch = useDispatch();
@@ -38,9 +41,11 @@ function ListDetails(props: param) {
     clearComponent();
   }
 
-  const updateCards = useCallback(() => {
+  const updateCards = useCallback(async () => {
     dispatch(updateAllCards({type: 'cards', cards: props.userList.cards}));
-  }, [props.userList.cards, dispatch]);
+    const editList = await theUserCanToEditTheList(props.userList);
+    setTheUserCanToEditListHis(editList);
+  }, [dispatch, props.userList]);
 
   useEffect(() => {
     updateCards();
@@ -62,54 +67,77 @@ function ListDetails(props: param) {
 
   return (
     <>
-      <Flex
-        bg="#312E81"
-        display="flex"
-        width="100%"
-        direction="row"
-        height="53px"
-        paddingTop={2}
-        paddingBottom={2}
-        justifyContent="space-between">
-        <Box width="91%" backgroundColor="#312E81">
-          <Input
-            paddingTop={3}
-            value={titleList}
-            onChangeText={(input: string) => updateListOfTitle(input)}
-            autoCorrect={false}
-            variant="underlined"
-            borderBottomColor="#312E81"
-            fontSize={16}
-            placeholderTextColor="#FFF"
-            fontWeight={600}
-            color="#FFF"
-            maxLength={30}
-            _focus={{
-              borderBottomColor: '#312E81',
-            }}
-          />
-        </Box>
-        <Box width="9%" flexDirection="column" justifyContent="center">
-          <Pressable
-            style={{
-              display: 'flex',
-              flex: 1,
-              flexDirection: 'column',
-              justifyContent: 'center',
-            }}
-            onPress={() => {
-              updateUserList();
-            }}>
-            <Done />
-          </Pressable>
-        </Box>
-      </Flex>
+      {theUserCanToEditListHis === true ? (
+        <Flex
+          bg="#312E81"
+          display="flex"
+          width="100%"
+          direction="row"
+          height="53px"
+          paddingTop={2}
+          paddingBottom={2}
+          justifyContent="space-between">
+          <Box width="91%">
+            <Input
+              paddingTop={3}
+              value={titleList}
+              onChangeText={(input: string) => updateListOfTitle(input)}
+              autoCorrect={false}
+              variant="underlined"
+              borderBottomColor="#312E81"
+              fontSize={16}
+              placeholderTextColor="#FFF"
+              fontWeight={600}
+              color="#FFF"
+              maxLength={30}
+              _focus={{
+                borderBottomColor: '#312E81',
+              }}
+            />
+          </Box>
+          <Box width="9%" flexDirection="column" justifyContent="center">
+            <Pressable
+              style={{
+                display: 'flex',
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+              }}
+              onPress={() => {
+                updateUserList();
+              }}>
+              <Done />
+            </Pressable>
+          </Box>
+        </Flex>
+      ) : (
+        <Flex
+          bg="#312E81"
+          display="flex"
+          width="100%"
+          direction="row"
+          height="53px"
+          paddingTop={2}
+          paddingBottom={2}
+          justifyContent="space-between">
+          <Box width="91%">
+            <Text
+              color="#FFF"
+              paddingTop={1}
+              fontSize={18}
+              fontWeight={600}
+              paddingLeft={3}>
+              {titleList}
+            </Text>
+          </Box>
+        </Flex>
+      )}
       <AlertPopover
         visible={visible}
         text={'Digite o titulo da lista'}
         setVisible={setVisible}
       />
-      <Cards />
+      <Cards isDisableButton={theUserCanToEditListHis} />
     </>
   );
 }
