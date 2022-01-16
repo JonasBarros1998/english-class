bd09b3e0ea7a436613e36e7e98ee046269c4384c
Jonas Barros <Jonas_barros@outlook.com>
2022-01-16 16:19:52 -0300

    adding new methods for load user datas on the storage

--- src/pubsub/reducers/userDatasLogged.ts
+++ src/pubsub/reducers/userDatasLogged.ts
@@ -1,5 +1,6 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
-import {storageGetItem} from '@storage/getItem';
+import {storageGetItem, storageSetItem} from '@storage/index';
+import {removeItem} from '@storage/removeItem';
 import {USER_STORAGE} from '@global/constants';
 
 const getUserDatasOnStorageAsync = createAsyncThunk(
@@ -10,10 +11,42 @@ const getUserDatasOnStorageAsync = createAsyncThunk(
   },
 );
 
+const removeUserDatasOnStorageAsync = createAsyncThunk(
+  'user/removeUserDatasOnStorage',
+  async () => {
+    await removeItem(USER_STORAGE);
+  },
+);
+
+const addUserDatasOnStorageAsync = createAsyncThunk(
+  'user/addUserDatasOnStorage',
+  async (payload: string) => {
+    await storageSetItem(USER_STORAGE, payload);
+    return {userDatas: JSON.parse(payload)};
+  },
+);
+
 export const userDatasLogged = createSlice({
   name: 'toLoadUserDatas',
   initialState: {
-    userData: {},
+    userData: {
+      user: {
+        email: null,
+        familyName: null,
+        givenName: null,
+        id: null,
+        name: null,
+        photo: null,
+      },
+      id: null,
+      scopes: [],
+      idToken: null,
+      uid: null,
+      lists: {
+        publicLists: [],
+        privateLists: [],
+      },
+    },
     status: 'pending',
   },
   reducers: {},
@@ -21,9 +54,41 @@ export const userDatasLogged = createSlice({
     builder.addCase(getUserDatasOnStorageAsync.fulfilled, (state, payload) => {
       return {userData: payload.payload.userDatas, status: 'complete'};
     });
+
+    builder.addCase(
+      removeUserDatasOnStorageAsync.fulfilled,
+      (state, payload) => {
+        const datas = {
+          user: {
+            email: null,
+            familyName: null,
+            givenName: null,
+            id: null,
+            name: null,
+            photo: null,
+          },
+          id: null,
+          scopes: [],
+          idToken: null,
+          uid: null,
+          lists: {
+            publicLists: [],
+            privateLists: [],
+          },
+        };
+        return {userData: datas, status: 'complete'};
+      },
+    );
+    builder.addCase(addUserDatasOnStorageAsync.fulfilled, (state, payload) => {
+      return {userData: payload.payload.userDatas, status: 'complete'};
+    });
   },
 });
 
-export {getUserDatasOnStorageAsync};
+export {
+  getUserDatasOnStorageAsync,
+  removeUserDatasOnStorageAsync,
+  addUserDatasOnStorageAsync,
+};
 
 export default userDatasLogged.reducer;
