59bbc80908544d5abc770faab4dbaef202036ce8
Jonas Barros <Jonas_barros@outlook.com>
2022-03-15 15:50:50 -0300

    get datas of the user in database

---
+++ src/modules/auth/services/saveUserInDatabase.ts
@@ -0,0 +1,32 @@
+import {insert} from '@services/database/repository/insert';
+import {select} from '@services/database/repository/search';
+import {userInfo} from '@global/types/userInfo';
+
+async function saveUserInDatabase(user: userInfo): Promise<void> {
+  if (typeof user === 'undefined') {
+    throw new TypeError('These parameters should be of userInfoType');
+  }
+  await insert([user], `users/${user.uid}`);
+}
+
+async function checkIfUserExistInDatabase(
+  uid: string,
+): Promise<userInfo[] | false> {
+  const loaduserDataOnFirebase: userInfo[] = [];
+
+  return select(`users/${uid}`)
+    .then(function (userData) {
+      if (userData.toJSON() === null) {
+        return false;
+      }
+      userData.forEach(function (item) {
+        loaduserDataOnFirebase.push(item.toJSON() as userInfo);
+      });
+      return loaduserDataOnFirebase;
+    })
+    .catch(function (error) {
+      throw new Error(error.message);
+    });
+}
+
+export {checkIfUserExistInDatabase, saveUserInDatabase};
