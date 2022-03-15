ee349eeccc365ca73c7da3afa255ec8fc0a4e7c6
Jonas Barros <Jonas_barros@outlook.com>
2022-03-15 17:10:40 -0300

    creting new module

--- babel.config.js
+++ babel.config.js
@@ -18,6 +18,7 @@ module.exports = {
           '@storage': './src/storage',
           '@services': './src/services',
           '@global': './src/global',
+          '@modules': './src/modules',
         },
       },
     ],
