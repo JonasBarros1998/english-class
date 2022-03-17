e252209c406af8ba536b300b8460256ce914aa1a
Jonas Barros <Jonas_barros@outlook.com>
2022-03-17 12:12:07 -0300

    create jsdocs

--- src/modules/auth/services/dispatchUserAction.ts
+++ src/modules/auth/services/dispatchUserAction.ts
@@ -1,6 +1,14 @@
 import store from '@pubsub/store';
 import {theUserHaveAccess} from '@pubsub/reducers/accessUserSlice';
 
+/**
+ * Dispatch action for especifield the user is authenticate this application
+ * not
+ * @param {boolean} value
+ * ```true``` = user is authenticate
+ *
+ * ```false``` = user isn't authenticate
+ */
 export function dispatchAction(value: boolean) {
   store.dispatch(theUserHaveAccess(value));
 }
