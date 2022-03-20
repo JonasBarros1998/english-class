f7b2dc752bbba03b96002221b2ba485540a82473
Jonas Barros <Jonas_barros@outlook.com>
2022-03-20 20:56:16 -0300

    creating new componet for search lists

---
+++ src/modules/lists/components/Search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SearchIcon, Box} from 'native-base';
+
+function Search() {
+  return (
+    <Box marginTop={'1'} marginRight={'4'}>
+      <SearchIcon size="6" color="white" />;
+    </Box>
+  );
+}
+
+export default Search;
