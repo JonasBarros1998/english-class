afe33711963589dfdd80a5ad0ce5309832b72613
Jonas <jonas_barros@outlook.com>
2022-05-22 15:17:36 -0300

    show a message when there aren't private lists

--- src/components/Lists/privateLists/PrivateList.tsx
+++ src/components/Lists/privateLists/PrivateList.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList, Pressable} from 'react-native';
 import {useDispatch} from 'react-redux';
-import {Box} from 'native-base';
+import {Box, Text} from 'native-base';
 import PrivateCards from '@components/Cards/PrivateCard';
 import {loadPrivateList} from './useCase/loadPrivateList';
 import {userList as typeUserList} from '@global/types/userList';
@@ -22,33 +22,41 @@ function PrivateList({navigation}: any) {
     [dispatch],
   );
   return (
-    <Box>
-      <FlatList
-        data={privateLists}
-        renderItem={({item, index}) => {
-          return (
-            <Pressable
-              onPress={() => {
-                navigation.navigate('listDetails', {
-                  screen: 'listDetails',
-                  listIsPublic: false,
-                  cardItem: item,
-                });
-              }}>
-              <PrivateCards card={item} />
-              {privateLists.length - 1 === index ? (
-                <>
-                  <Box marginBottom={'32'} />
-                </>
-              ) : (
-                <></>
-              )}
-            </Pressable>
-          );
-        }}
-        keyExtractor={({id}, key) => String(key)}
-      />
-    </Box>
+    <>
+      {privateLists.length === 0 ? (
+        <Text fontSize="md" textAlign={'center'}>
+          Suas listas privadas apareceram aqui
+        </Text>
+      ) : (
+        <Box>
+          <FlatList
+            data={privateLists}
+            renderItem={({item, index}) => {
+              return (
+                <Pressable
+                  onPress={() => {
+                    navigation.navigate('listDetails', {
+                      screen: 'listDetails',
+                      listIsPublic: false,
+                      cardItem: item,
+                    });
+                  }}>
+                  <PrivateCards card={item} />
+                  {privateLists.length - 1 === index ? (
+                    <>
+                      <Box marginBottom={'32'} />
+                    </>
+                  ) : (
+                    <></>
+                  )}
+                </Pressable>
+              );
+            }}
+            keyExtractor={({id}, key) => String(key)}
+          />
+        </Box>
+      )}
+    </>
   );
 }
 
