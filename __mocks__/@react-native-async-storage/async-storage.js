e8c5a6f2e16776b40ac0c28a482a4ad04f62dbf5
Jonas <jonas_barros@outlook.com>
2022-06-28 20:29:38 -0300

    settings jest and storage

--- __mocks__/@react-native-async-storage/async-storage.js
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -1,6 +1,5 @@
-// import mockAsyncStorage from '@react-native-async-storage/async-storage';
+export default from "@react-native-async-storage/async-storage/jest/async-storage-mock";
 
-// jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
-export default from '@react-native-async-storage/async-storage/jest/async-storage-mock'
-
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
