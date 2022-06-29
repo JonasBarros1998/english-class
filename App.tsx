0852e6e6c5d1d5812e56784582a4850859ee1b13
Jonas <jonas_barros@outlook.com>
2022-06-28 22:36:16 -0300

    provider store

--- App.tsx
+++ App.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
+import {StatusBar} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Provider} from 'react-redux';
 import {theme} from '@theme/index';
-import store from 'src/state/redux/store';
-import {NavigationContainer} from '@react-navigation/native';
-import Login from '@auth/components/Login';
+import Routes from 'src/routes/route';
+import store from "@state/redux/store";
 
 const App = () => {
   return (
-    /**
-    <Provider>
-    */
-    <PaperProvider theme={theme}>
-      <SafeAreaView>
-        <NavigationContainer>
-          <StatusBar />
-          <Login />
-        </NavigationContainer>
-      </SafeAreaView>
-    </PaperProvider>
-    /**
-    </Provider>
-    */
+    <>
+      <Provider store={store}>
+      <PaperProvider theme={theme}>
+        <Routes />
+        <StatusBar />
+      </PaperProvider>
+      </Provider>
+    </>
   );
 };
 
