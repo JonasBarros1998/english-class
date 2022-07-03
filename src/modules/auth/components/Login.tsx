e87dc9e3140a259e60d378edc5cccdff8104b343
Jonas <jonas_barros@outlook.com>
2022-07-02 21:45:34 -0300

    connection app

--- src/modules/auth/components/Login.tsx
+++ src/modules/auth/components/Login.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import {View} from 'react-native';
 import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
-import {styles} from '../../styles/login';
+import {styles} from '../styles/login';
+import {userLogin} from '../useCases/auth';
 
-export default function Login() {
+export default function Login(props: any) {
   return (
     <View style={{...styles.container}}>
       <GoogleSigninButton
         size={GoogleSigninButton.Size.Wide}
         color={GoogleSigninButton.Color.Dark}
-        onPress={() => console.log('jonas')}
+        onPress={() => {
+          userLogin(props.navigation);
+        }}
       />
     </View>
   );
