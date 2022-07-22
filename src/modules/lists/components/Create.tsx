765c23d073ff18e2f628a832620d69536282c46d
Jonas <jonas_barros@outlook.com>
2022-07-21 22:23:28 -0300

    add new componets

--- src/modules/lists/components/Create.tsx
+++ src/modules/lists/components/Create.tsx
@@ -1,72 +1,90 @@
 import { Card } from '@global/interfaces/Card';
 import React, { useState } from 'react';
-import { Button, FlatList, View } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { createNewCard, updateInputCards } from '../useCases/addNewCardInList';
 import AddNewCardButton from './AddNewCardButton';
-import CardItem from './card';
+import SaveListButton from './SaveListButton';
+import CardItem from './Card';
+import {styles} from '../styles/titleList';
+import {useTheme} from 'react-native-paper';
+import TitleList from './TitleList';
 
 export function Create() {
   const [cards, setCards] = useState<Card[]>([createNewCard()]);
+  const theme = useTheme() as any;
 
   return (
-    <View style={{
-      display: 'flex',
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'space-between'
-    }}>
-      <FlatList 
-        data={cards}
-        keyExtractor={({id}) => String(id)}
-        renderItem={({item, index}) => {
-          return (
-            <CardItem
-              currentItem={index}
-              data={cards}
-              onChangeInputWord={(value: any) => {
-                updateInputCards({
-                  cards,
-                  cardId: item.id,
-                  input: {
-                    value,
-                    name: "word"
-                  }
-                });
-              }}
-              onChangeInputContext={(value: string) => {
-                updateInputCards({
-                  cards,
-                  cardId: item.id,
-                  input: {
-                    value,
-                    name: "context"
-                  }
-                });
-              }}
-              onChangeInputTranslation={(value: string) => {
-                updateInputCards({
-                  cards,
-                  cardId: item.id,
-                  input: {
-                    value,
-                    name: "translation"
-                  }
-                });
-              }}
-            />
-          )
-        }} 
-      />
-      
-      <AddNewCardButton 
-        updateState={(card: any) => {
-          const cardsArray: Card[] = [];
-          cardsArray.push(...cards as any);
-          cardsArray.push(card);
-          setCards([...cardsArray]);
-        }}
-      />
+    <>
+      <View
+        style={{
+          ...styles.actionList,
+          backgroundColor: theme.colors.primary,
+          height: 50,
+          paddingLeft: 10
+        }}>
+        <TitleList />
+        <SaveListButton />
+      </View>
 
-    </View>
+      <View style={{
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+      }}>
+        <FlatList 
+          data={cards}
+          keyExtractor={({id}) => String(id)}
+          renderItem={({item, index}) => {
+            return (
+              <CardItem
+                currentItem={index}
+                data={cards}
+                onChangeInputWord={(value: any) => {
+                  updateInputCards({
+                    cards,
+                    cardId: item.id,
+                    input: {
+                      value,
+                      name: "word"
+                    }
+                  });
+                }}
+                onChangeInputContext={(value: string) => {
+                  updateInputCards({
+                    cards,
+                    cardId: item.id,
+                    input: {
+                      value,
+                      name: "context"
+                    }
+                  });
+                }}
+                onChangeInputTranslation={(value: string) => {
+                  updateInputCards({
+                    cards,
+                    cardId: item.id,
+                    input: {
+                      value,
+                      name: "translation"
+                    }
+                  });
+                }}
+              />
+            )
+          }} 
+        />
+        
+        <AddNewCardButton 
+          updateState={(card: any) => {
+            const cardsArray: Card[] = [];
+            cardsArray.push(...cards as any);
+            cardsArray.push(card);
+            setCards([...cardsArray]);
+          }}
+        />
+
+      </View>
+    </>
   )
 }
