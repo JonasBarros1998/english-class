b05a0688e6e6e457d221184ffbeb80fee0d09068
Jonas <jonas_barros@outlook.com>
2022-07-31 21:51:53 -0300

    crenew new test for the params component

--- __tests__/modules/lists/components/AddNewCardButton.test.tsx
+++ __tests__/modules/lists/components/AddNewCardButton.test.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { render, fireEvent } from '@testing-library/react-native';
 import AddNewCardButton from '@src/modules/lists/components/AddNewCardButton';
 import { createNewCard } from '../../../../src/modules/lists/useCases/addNewCardInList';
+import { Card } from '@global/interfaces/Card';
 
 jest.mock('../../../../src/modules/lists/useCases/addNewCardInList');
 
 jest.mock('react-native-paper', () => ({
   __esModule: true,
   IconButton: () => 'IconButton',
-  useTheme: () => 'useTheme'
+  useTheme: () => ({
+    colors: {
+      primary: "black"
+    }
+  })
 }));
 
 const createNewCardMock = createNewCard as any;
@@ -20,20 +25,63 @@ createNewCardMock.mockImplementation(() => ({
   context: ''
 }));
 
+const updateState = jest.fn((card: Card) => {});
+
 describe('AddNewCardButton', function() {
-  test('WhenPressButtonShouldCallFunctionCreateNewCard', function() {
+
+  beforeEach(() => {
     const {getByTestId} = render(
       <AddNewCardButton 
         updateState={() => {}}
       />
     );
-    
+
+    getByTestId('button').children.map((value) => {
+      if (typeof value !== 'string') {
+        fireEvent.press(value);
+      }
+    });
+  });
+
+  test('WhenPressButtonShouldCallFunctionCreateNewCard', function() {
+    expect(createNewCard).toHaveBeenCalledTimes(1);
+  });
+
+  test('WhenPressButtonShouldCallFunctionUpdateStateWithObjectForAddNewCard', function() {
+    const {getByTestId} = render(
+      <AddNewCardButton 
+        updateState={updateState}
+      />
+    );
+
     getByTestId('button').children.map((value) => {
       if (typeof value !== 'string') {
         fireEvent.press(value);
       }
     });
 
-    expect(createNewCard).toHaveBeenCalledTimes(1);
+    expect(updateState).toHaveBeenCalledWith({
+      id: "1234",
+      word: '',
+      translation: '',
+      context: ''
+    });
   });
+
+  test("WhenPressButtonShouldCallFunctionUpdateState", function() {
+    const {getByTestId} = render(
+      <AddNewCardButton 
+        updateState={updateState}
+      />
+    );
+
+    getByTestId('button').children.map((value) => {
+      if (typeof value !== 'string') {
+        fireEvent.press(value);
+      }
+    });
+
+    expect(updateState).toHaveBeenCalledTimes(1);
+  });
+
 });
