99859f8ddd6b29b9b9704d13c13bd082f696bb0e
Jonas <jonas_barros@outlook.com>
2022-09-17 21:08:37 -0300

    componente para listar cards

--- src/modules/lists/components/Lists.tsx
+++ src/modules/lists/components/Lists.tsx
@@ -1,10 +1,60 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
+import { useTheme } from 'react-native-paper';
+
+import { findAllLists } from '../useCases/readLists';
+import { List } from '@global/interfaces/Card';
+
+import { styles } from '../styles/cards';
+import {fonts} from '@theme/index';
 
 export function Lists() {
+  const [lists, setLists] = useState<List[]>();
+  const [refresh, setRefresh] = useState<boolean>(false);
+
+  useEffect(() => {
+    findAllLists()
+      .then(function(response) {
+        setLists([...response]);
+      })
+  }, []);
+
+  async function refreshList() {
+    setRefresh(true);
+    findAllLists()
+      .then(function(response) {
+        setLists([...response]);
+      })
+      .catch(function() {
+        setRefresh(false)
+      })
+      .finally(() => {
+        setRefresh(false);
+      });
+  }
+
+  const theme = useTheme();
+  const css = styles(theme);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Render Lists</Text>
+    <View>
+      <FlatList
+        data={lists}
+        refreshing={refresh}
+        onRefresh={() => refreshList()}
+        renderItem={({item, index}) => {
+          return (
+            <View style={{...css.container}}>
+              <View style={{...css.card}}>
+                <Text style={{
+                  ...css.cardInfo, 
+                  ...fonts.subTitle,
+                  }}>{item.title}</Text>
+              </View>
+            </View>
+          )
+        }}
+      />
     </View>
   )
 }
