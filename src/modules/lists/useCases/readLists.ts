6431a4a0371c8ef2801b620e8b6c2094a2c47272
Jonas <jonas_barros@outlook.com>
2022-11-28 20:59:34 -0300

    test fix

--- src/modules/lists/useCases/readLists.ts
+++ src/modules/lists/useCases/readLists.ts
@@ -5,6 +5,7 @@ import { dispatchAllListToStore } from "./dispatchListToStore";
 export async function findAllLists(): Promise<void> {
   return await findAll(collections.lists)
     .then(function(response) {
+      console.log(response);
       dispatchAllListToStore(response);
     })
     .catch((error) => {throw error;})
