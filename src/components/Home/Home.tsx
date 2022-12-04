04bca7a5851643fadf902fc8c2f3ee6162639ad9
Jonas <jonas_barros@outlook.com>
2022-12-04 15:33:11 -0300

    - create layout for home screen - adjusting List component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,16 +1,76 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { FlatList, Pressable, Text, View } from 'react-native';
+import { useTheme } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 
-export default function Home() {
-  
-  useSelector((store) => {
-    //console.log(store);
+import { List } from '@global/interfaces/Card';
+import {navigateToListDetails} from '@modules/lists/routes/routes';
+import {styles} from '@modules/lists/styles/cards';
+import {findAllLists} from '@modules/lists/useCases/readLists';
+import {styles as homeStyles} from './styles/main';
+import { dispatchCurrentListToStore } from '@modules/lists/useCases/dispatchListToStore';
+
+export default function Home({navigation}: {navigation: (route: string) => any}) {
+
+  const listeningLists = useSelector<{readList: {allLists: List[]}}, List[]>((items) => items.readList.allLists);
+  const [loading, setLoading] = useState(false);
+
+  const readFiveList = listeningLists.filter((list, index) => {
+    if (index <= 9) {
+      return list;
+    }
   });
 
+  useEffect(() => {
+    findAllLists()
+      .then(() => setLoading(true))
+      .catch(() => setLoading(false));
+    
+  }, []);
+
+  function onClickEvent(list: List) {
+    dispatchCurrentListToStore(list);
+    navigateToListDetails(navigation, list.id);
+  }
+
+  const theme = useTheme();
+  const css = styles(theme);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home</Text>
-    </View>
+    <>
+    {
+      loading === false ? (
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Carregando listas...</Text>
+        </View>
+        ): (
+        <View>
+          <Text style={{
+            ...homeStyles().title
+          }}>Principais listas</Text>
+          <FlatList
+            testID='card'
+            data={readFiveList}
+            contentContainerStyle={homeStyles().fotter}
+            renderItem={({item}) => {
+              return (
+                <Pressable onPress={() => onClickEvent(item)}>
+                  <View style={{...css.container}} testID="content">
+                    <View style={{...css.card}}>
+                      <Text style={{
+                        ...css.cardInfo, 
+                        ...css.subTitle
+                        }}>{item.title}</Text>
+                    </View>
+                  </View>
+                </Pressable>
+              )
+            }}
+          />
+        </View>
+      )
+    }
+  
+    </>
   );
 }
--- src/modules/lists/useCases/readLists.ts
+++ src/modules/lists/useCases/readLists.ts
@@ -5,7 +5,6 @@ import { dispatchAllListToStore } from "./dispatchListToStore";
 export async function findAllLists(): Promise<void> {
   return await findAll(collections.lists)
     .then(function(response) {
-      console.log(response);
       dispatchAllListToStore(response);
     })
     .catch((error) => {throw error;})
