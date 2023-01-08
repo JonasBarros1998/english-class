5d1f434c953918070c79576593d16f5d085977a4
Jonas <jonas_barros@outlook.com>
2023-01-08 20:22:54 -0300

    create new header for home page

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Pressable} from 'react-native';
 import {styles} from './styles/main';
+import {useTheme} from 'react-native-paper';
+import {navigateToHelpForm} from '@modules/Form/routes/routes';
 
 
-export default function Header() {
-  const {header, titleLeft} = styles();
+export default function Header({navigation}: any) {
+  const theme = useTheme()
+  const {header, titleLeft, opnion, opnionText} = styles(theme.colors);
 
   return(
     <View style={{...header}}>
       <Text style={{...titleLeft}}>Inicio</Text>
-      <Text>Help</Text>
+      <Pressable onPress={() => navigateToHelpForm(navigation)}>
+        <View style={{...opnion}}>
+          <Text style={{...opnionText}}>Dê a sua opnião</Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
