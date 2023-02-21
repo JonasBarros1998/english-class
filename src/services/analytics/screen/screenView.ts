a9904430298e3f9b8c097b09cd9b6694a393e2c2
Jonas <jonas_barros@outlook.com>
2023-02-21 16:58:31 -0300

    create new tag for tracking pages in app

--- src/services/analytics/screen/screenView.ts
+++ src/services/analytics/screen/screenView.ts
@@ -1,4 +1,5 @@
 import analytics from '@react-native-firebase/analytics';
+import {selectEnvironment} from '../environment';
 
 type description = {
   screenName: string,
@@ -6,7 +7,12 @@ type description = {
 }
 
 export async function screenView(descriptionScreen: description) {
-  return analytics().logScreenView(descriptionScreen)
+  const screen = Object.assign<description, {}>(descriptionScreen, {});
+
+  return analytics().logScreenView({
+    screen_class: selectEnvironment(screen.screenClass) as string,
+    screen_name: selectEnvironment(screen.screenName) as string
+  })
     .catch(function(error) {
       console.log(`ANALYTICS ERROR [${descriptionScreen.screenName}]`);
       console.error(error.message);
