da457c25e0f72ed5f989b53e7d34596c58927882
Jonas <jonas_barros@outlook.com>
2023-04-09 14:37:40 -0300

    create new slice

---
+++ src/state/redux/slices/flashcards.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction} from "@reduxjs/toolkit";
+import { FlashCard } from "@global/interfaces/FlashCard";
+
+const flashcards = createSlice({
+  name: 'flashcards',
+  initialState: {
+    flashcards: [] as FlashCard[]
+  },
+
+  reducers: {
+    addNewFlashCard: {
+      reducer:(state, action: PayloadAction<FlashCard>) => {
+        
+        state.flashcards.push(action.payload);
+      },
+      prepare: (datas: FlashCard) => {
+        console.log("FlashCard >> ", datas)
+        return {payload: datas}
+      }
+    },
+
+    updateFlashCard: {
+      reducer: (state, action: PayloadAction<FlashCard[]>) => {
+        return {
+          ...state,
+          flashcards: action.payload
+        }
+      },
+
+      prepare: (datas: FlashCard[]) => {
+        console.log("updateFlashCard >> ", datas)
+        return {payload: datas};
+      }
+    }
+  }
+});
+
+export const {addNewFlashCard, updateFlashCard} = flashcards.actions
+export default flashcards.reducer;
--- src/state/redux/store.ts
+++ src/state/redux/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import user from './slices/user';
 import readList from './slices/readList';
+import flashcards from './slices/readList';
 
 export default configureStore({
   reducer: {
     user,
-    readList
+    readList,
+    flashcards
   },
 });
